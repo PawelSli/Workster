98bb5c53aa3ccb57e39d4b224894b68fc9975e33
PawelSli <ps439787@gmail.com>
2021-09-06 23:35:03 +0200

    Initial creation of project.

---
+++ src/main/java/com/github/pawelsli/workster/WorksterApplication.java
@@ -0,0 +1,13 @@
+package com.github.pawelsli.workster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorksterApplication.class, args);
+    }
+
+}
