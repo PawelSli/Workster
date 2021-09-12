d8aa6a398c41a098901dddede4eeca75523942e5
PawelSli <ps439787@gmail.com>
2021-09-12 19:29:11 +0200

    Adding correct routing and initial main page.

--- workster-frontend/src/App.js
+++ workster-frontend/src/App.js
@@ -1,24 +1,49 @@
 import './App.css';
 import {Route, Switch} from "react-router";
-import { Router} from 'react-router-dom';
+import {Router} from 'react-router-dom';
 import PrivateRoute from "./components/PrivateRoute";
+import React from "react";
+import Navigation from "./components/Navigation";
+import MainPage from "./components/MainPage";
+import history from "./hooks/history";
+import Scripts from "./hooks/Scripts";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/add-job-description.css';
+import './assets/styles/add-job-post.css';
+import './assets/styles/Bootstrap-4---Product-List.css';
+import './assets/styles/Google-Style-Login.css';
+import './assets/styles/ion.rangeSlider.css';
+import './assets/styles/job-list.css';
+//import './styles/Login-Form-Dark.css';
+import './assets/styles/Navigation-with-Search.css';
+import './assets/styles/Pretty-Login-Form.css';
+import './assets/styles/Pretty-Registration-Form.css';
+import './assets/styles/profile.css';
+//import './styles/Registration-Form-with-Photo.css';
+import './assets/styles/Table-With-Search.css';
+import './assets/styles/Table-With-Search-1.css';
+import './assets/styles/vanilla-zoom.min.css';
+
+import './assets/js/bs-init';
+//import './assets/js/multiple';
+import './assets/js/range';
+import './assets/js/Table-With-Search';
+import './assets/js/vanilla-zoom';
+
+import $ from "jquery";
 
 function App() {
+
     return (
-        <div>
-            <Router>
+        <>
+            <Scripts/>
+            <Router history={history}>
+                <Navigation/>
                 <Switch>
-                    <PrivateRoute exact path="/addDish"/>
-                    <PrivateRoute exact path="/profile"/>
-                    <Route exact path="/api/auth/signup"/>
-                    <Route exact path="/api/auth/signin"/>
-                    <Route exact path="/search"/>
-                    <Route exact path="/recipe/*"/>
-                    <Route exact path="/"/>
+                    <Route exact path="/" component={MainPage}/>
                 </Switch>
             </Router>
-
-        </div>
+        </>
     );
 }
 
---
+++ workster-frontend/src/hooks/Scripts.js
@@ -0,0 +1,30 @@
+export default function Scripts() {
+
+    return (
+        <div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" defer/>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" defer/>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js" defer/>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.js" defer/>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.min.js" defer/>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.full.min.js" defer/>
+
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+            <link rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css"/>
+            <link rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.css"/>
+            <link rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css"
+                  defer/>
+
+            <link rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.min.js"/>
+
+        </div>
+    );
+}
