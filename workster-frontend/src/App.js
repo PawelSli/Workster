948460f9ba1f0b303b87644535b3a34dbecdeadc
PawelSli <ps439787@gmail.com>
2021-10-03 14:57:08 +0200

    add registration and login pages and all assects and dependencies associated with these pages.

--- workster-frontend/src/App.js
+++ workster-frontend/src/App.js
@@ -3,24 +3,28 @@ import {Router} from 'react-router-dom';
 import React from "react";
 import Navigation from "./components/Navigation";
 import MainPage from "./components/MainPage";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
 import history from "./hooks/history";
-import Scripts from "./hooks/Scripts";
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/job-list.css';
 import './assets/styles/profile.css';
 import './assets/styles/navigation.css';
+import './assets/styles/login-form.css';
+import './assets/styles/registration-form.css';
 
 function App() {
 
     return (
         <>
-            <Scripts/>
             <Router history={history}>
                 <Navigation/>
                 <Switch>
                     <Route exact path="/" component={MainPage}/>
+                    <Route exact path="/login" component={LoginPage}/>
+                    <Route exact path="/register" component={RegisterPage}/>
                 </Switch>
             </Router>
         </>
---
+++ workster-frontend/src/components/LoginPage.js
@@ -0,0 +1,25 @@
+import { Icon } from '@iconify/react';
+
+
+export default function LoginPage() {
+
+    return (
+        <main className="page login-page">
+            <section className="login-dark">
+                <form method="post">
+                    <h2 className="visually-hidden">Login Form</h2>
+                    <div className="illustration"><Icon icon="ion-ios-locked-outline" /></div>
+                    <div className="mb-3"><input className="form-control" type="email" name="email" placeholder="Email"/>
+                    </div>
+                    <div className="mb-3"><input className="form-control" type="password" name="password"
+                                                 placeholder="Password"/></div>
+                    <div className="mb-3">
+                        <button className="btn btn-primary d-block w-100" type="submit">Log In</button>
+                    </div>
+                    <a className="forgot" href="#">Forgot your email or password?</a>
+                </form>
+            </section>
+        </main>
+    )
+
+};
