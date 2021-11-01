dc46dcadc1b49fc2c9e5a0bf402712cc2b967475
PawelSli <ps439787@gmail.com>
2021-11-01 13:40:11 +0100

    edit contact-us page.

--- workster-frontend/src/components/pages/ContactUs.js
+++ workster-frontend/src/components/pages/ContactUs.js
@@ -1,23 +1,40 @@
+import CreatableSingleSearch from "../reusable/CreatableSingleSearch";
+import React from "react";
+
 export default function ContactUs() {
+
+    const options = [
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'strawberry', label: 'Strawberry'},
+        {value: 'vanilla', label: 'Vanilla'},
+    ];
 
     return (
         <main>
             <section className="clean-block clean-form dark">
                 <div className="container">
                     <div className="block-heading">
-                        <h2 className="text-dark">Contact Us</h2>
+                        <h2 className="text-dark">Contact us</h2>
                         <p>Fill out the information below to contact portal support.</p>
                     </div>
-                    <form>
-                        <div className="mb-3"><label className="form-label" htmlFor="name">Name</label><input
-                            type="text" className="form-control" id="name-1" name="name"/></div>
-                        <div className="mb-3"><label className="form-label" htmlFor="subject">Subject</label><input
-                            type="text" className="form-control" id="subject-1" name="subject"/></div>
-                        <div className="mb-3"><label className="form-label" htmlFor="email">Email</label><input
-                            type="email" className="form-control" id="email-1" name="email"/></div>
-                        <div className="mb-3"><label className="form-label" htmlFor="message">Message</label><textarea
-                            className="form-control" id="message-1" name="message"/></div>
+                    <form className="job-desc-form-form shadow-lg">
                         <div className="mb-3">
+                            <label className="form-label" htmlFor="name">Name</label>
+                            <input type="text" className="form-control" id="name-1" name="name"/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="subject">Subject</label>
+                            <CreatableSingleSearch valueOptions={options} />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="email">Email</label>
+                            <input type="email" className="form-control" id="email-1" name="email"/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="message">Message</label>
+                            <textarea className="form-control" id="message-1" name="message"/>
+                        </div>
+                        <div className="mb-3 text-center">
                             <button className="btn btn-primary" type="submit">Send</button>
                         </div>
                     </form>
