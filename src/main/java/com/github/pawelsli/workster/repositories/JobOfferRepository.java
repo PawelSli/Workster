804eb1dc063351293f45d26f5be973a01f171ce4
PawelSli <ps439787@gmail.com>
2021-11-29 20:25:19 +0100

    allow to display list of favourite job offers.

--- src/main/java/com/github/pawelsli/workster/repositories/JobOfferRepository.java
+++ src/main/java/com/github/pawelsli/workster/repositories/JobOfferRepository.java
@@ -2,6 +2,7 @@ package com.github.pawelsli.workster.repositories;
 
 import com.github.pawelsli.workster.entities.Company;
 import com.github.pawelsli.workster.entities.JobOffer;
+import com.github.pawelsli.workster.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,8 @@ import java.util.List;
 public interface JobOfferRepository extends JpaRepository<JobOffer, Long> {
 
     List<JobOffer> findAllByCompany(Company company);
+
+    List<JobOffer> findAllByFansContaining(User user);
 
     JobOffer findByTitle(String title);
 }
